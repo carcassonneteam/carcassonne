4a6417e0615948edf183c9f0b9914a3477a99ec2
Arek_S <arek561@gmail.com>
2018-02-17 13:27:54 +0100

    Game players setup

---
+++ src/game/game.js
@@ -0,0 +1,22 @@
+(function($, window) {
+
+	var players = [];
+
+	window.Game = function() {
+		window.gameController = this;
+		players = [];
+	};
+
+	window.Game.prototype.showScreen = function(screen) {
+		$("#game").load('screens/' + screen + ".html");
+	};
+
+	window.Game.prototype.addPlayer = function(player) {
+		players.push(player);
+	};
+
+	window.Game.prototype.getPlayers = function() {
+		return players;
+	};
+
+})(jQuery, window);
